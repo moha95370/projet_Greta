0df923046170724377013b38bceb705a4e7aea72
Ouada Mohamed <124060190+moha95370@users.noreply.github.com>
2023-12-21 10:32:40 +0100

    organisations

--- src/Controller/Dashboard/DashboardController.php
+++ src/Controller/Dashboard/DashboardController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Dashboard;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/dashboard', name: 'dashboard_')]
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
